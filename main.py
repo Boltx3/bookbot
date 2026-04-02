11fc8ab75c89588cc521c06591b4395129b4a330
David <dav.h.pham@gmail.com>
2026-04-02 12:45:17 -0500

    refactored

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ def main():
         sys.exit(1)
 
     file_contents = read_string()
-#   print_report(file_contents)
     word_count(file_contents)
     new_contents = char_count(file_contents)
     print_list_dict(dict_to_list_dicts(new_contents))
@@ -24,28 +23,5 @@ def read_string():
         file_contents = f.read()
         return file_contents
 
-#def dict_to_list_dicts(dictionary):
-#    return [{"key": key, "value": dictionary[key]} for key in dictionary]
-
-#def sort_on(dict):
-#    return dict["value"]
-
-#def print_list_dict(list_dict):
-#    list_dict.sort(reverse=True, key=sort_on)
-#    for d in list_dict:
-#        char = d["key"]
-#        count = d["value"]
-#        if char.isalpha():
-#            print(f"The '{char}' character was found '{count}' times")
-
-#def print_report(file_contents):
-#    print (f"--- Begin Report of books/frankenstein.txt --")
-#    wc = word_count(file_contents)
-#    print (f"{wc} words found in the document\n")
-#    char_dict = char_count(file_contents)
-#    char_dict_list = dict_to_list_dicts(char_dict)
-#    print_list_dict(char_dict_list)
-
- 
 main()
 
